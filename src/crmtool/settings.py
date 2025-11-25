2e98ef8a7d7a4d423d3ad8b0c90b0246963fbc43
AmoKiswaya <amosonganga254@gmail.com>
2025-11-25 10:23:16 +0300

    Add whitenoise
    Manage static files with Django on settings.py

--- src/crmtool/settings.py
+++ src/crmtool/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -60,10 +61,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "crmtool.urls"
 
+TEMPLATES_DIR = BASE_DIR / "templates"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATES_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -124,6 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+# git don't track these files
+STATIC_ROOT = BASE_DIR / "local-cdn"
+
+STATICFILES_DIRS = [BASE_DIR / "mystaticfiles"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
